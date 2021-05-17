39b293d41b7dcefd6df8a42d49216f61ce8b05b4
janfaron <faron.jan@gmail.com>
2021-05-17 16:39:56 +0200

    [#177918193] make E2E_ACTUATOR_INFO_NAME optional

--- src/test/java/com/dnastack/test/actuator/ActuatorE2eTest.java
+++ src/test/java/com/dnastack/test/actuator/ActuatorE2eTest.java
@@ -27,6 +27,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -34,12 +35,12 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 public class ActuatorE2eTest {
 
     protected static final String BASE_URI = requiredEnv("E2E_BASE_URI");
-    protected static final String ACTUATOR_INFO_NAME = requiredEnv("E2E_ACTUATOR_INFO_NAME");
+    protected static final String ACTUATOR_INFO_NAME = getEnv("E2E_ACTUATOR_INFO_NAME");
     protected static final Boolean ACCESS_TOKEN_AUTH_ENABLED = Boolean.parseBoolean(getEnv("E2E_ACCESS_TOKEN_AUTH_ENABLED"));
     protected static final Boolean COOKIE_AUTH_ENABLED = Boolean.parseBoolean(getEnv("E2E_COOKIE_AUTH_ENABLED"));
     protected static final Boolean REDIRECT_ENABLED = Boolean.parseBoolean(getEnv("E2E_REDIRECT_ENABLED"));
     protected static final String LOGIN_URL_PATH = optionalEnv("E2E_LOGIN_URL_PATH", "/");
-    protected static final String WALLET_URL = optionalEnv("E2E_WALLET_URL", "http://localhost:8081");
+    protected static final String WALLET_TOKEN_URI = optionalEnv("E2E_WALLET_TOKEN_URI", "http://localhost:8081/oauth/token");
     protected static final String WALLET_CLIENT_ID = getEnv("E2E_WALLET_CLIENT_ID");
     protected static final String WALLET_CLIENT_SECRET = optionalEnv("E2E_WALLET_CLIENT_SECRET", "dev-secret-never-use-in-prod");
 
@@ -65,16 +66,24 @@ public class ActuatorE2eTest {
     protected void appNameAndVersionShouldBeExposed(String nameKey, String versionKey) {
         assumeFalse(localDeploymentUrl().matches(BASE_URI), "Service info isn't set on local dev builds");
 
-        given()
+        JsonPath jsonPath = given()
             .log().method()
             .log().uri()
-            .when()
+        .when()
             .get("/actuator/info")
-            .then()
+        .then()
             .log().ifValidationFails()
             .statusCode(200)
-            .body(nameKey, equalTo(ACTUATOR_INFO_NAME))
-            .body(versionKey, notNullValue());
+            .extract().jsonPath();
+
+        String name = jsonPath.getString(nameKey);
+        String version = jsonPath.getString(versionKey);
+        if (ACTUATOR_INFO_NAME != null && !ACTUATOR_INFO_NAME.isEmpty()){
+            assertThat(name, equalTo(ACTUATOR_INFO_NAME));
+        } else {
+            assertTrue(name != null && !name.isEmpty());
+        }
+        assertTrue(version != null && !version.isEmpty());
     }
 
     @Test
@@ -134,12 +143,12 @@ public class ActuatorE2eTest {
             .forEach(endpoint -> {
                 final Response response = given()
                     .when()
-                    .cookies(toRestAssuredCookies(cookies))
-                    .redirects().follow(false)
-                    .get("/actuator/" + endpoint)
+                        .cookies(toRestAssuredCookies(cookies))
+                        .redirects().follow(false)
+                        .get("/actuator/" + endpoint)
                     .then()
-                    .extract()
-                    .response();
+                        .extract()
+                        .response();
                 MatcherAssert.assertThat(
                     format("Actuator endpoint '%s' is being exposed or misconfigured! Returning status code %s", endpoint, response.getStatusCode()),
                     response.statusCode(), equalTo(404)
@@ -159,12 +168,12 @@ public class ActuatorE2eTest {
             .forEach(endpoint -> {
                 final Response response = given()
                     .when()
-                    .auth().oauth2(accessToken)
-                    .redirects().follow(false)
-                    .get("/actuator/" + endpoint)
+                        .auth().oauth2(accessToken)
+                        .redirects().follow(false)
+                        .get("/actuator/" + endpoint)
                     .then()
-                    .extract()
-                    .response();
+                        .extract()
+                        .response();
                 assertThat(
                     format("Actuator endpoint '%s' is being exposed or misconfigured! Returning status code %s", endpoint, response.getStatusCode()),
                     response.statusCode(), equalTo(404)
@@ -239,12 +248,12 @@ public class ActuatorE2eTest {
 
     protected String getBearerToken(String requiredResource) {
         Objects.requireNonNull(requiredResource);
-        Objects.requireNonNull(WALLET_URL);
+        Objects.requireNonNull(WALLET_TOKEN_URI);
         Objects.requireNonNull(WALLET_CLIENT_ID);
         Objects.requireNonNull(WALLET_CLIENT_SECRET);
 
         RequestSpecification specification = new RequestSpecBuilder()
-            .setBaseUri(WALLET_URL)
+            .setBaseUri(WALLET_TOKEN_URI)
             .build();
 
         RequestSpecification reqSpec = given(specification)
@@ -258,11 +267,11 @@ public class ActuatorE2eTest {
 
         JsonPath tokenResponse = reqSpec
             .when()
-            .post()
+                .post()
             .then()
-            .log().ifValidationFails()
-            .statusCode(200)
-            .extract().jsonPath();
+                .log().ifValidationFails()
+                .statusCode(200)
+                .extract().jsonPath();
 
         return tokenResponse.getString("access_token");
     }
